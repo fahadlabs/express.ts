6e8f099d0af72e581996369c8d9b21a19a350c74
Fahad Hossain <fahad@avalonx.io>
2021-09-01 01:57:16 +0600

    Initial setup

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import express, { Request, Response } from 'express';
+import cookieParser from 'cookie-parser';
+import createError from 'http-errors';
+import logger from 'morgan';
+import path from 'path';
+
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+
+const app = express();
+
+app.disable('x-powered-by');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use((_req: Request, _res: Response, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err: any, req: Request, res: Response) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+export default app;
---
+++ src/tests/index.spec.ts
@@ -0,0 +1,5 @@
+describe('My Test Suit', () => {
+  test('should pass', () => {
+    expect(true).toEqual(true);
+  });
+});
